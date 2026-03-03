42e7656ca4059190e7bbf254bade76653853c07c
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-02 23:46:56 -0500

    Add files via upload

---
+++ pin.py
@@ -0,0 +1,8 @@
+x= ""
+while True:
+    y = int(input("Please Enter Pin:"))
+    if y == 1234:
+        print("That's correct")
+        break
+    else:
+        print("Sorry that's the wrong pin.")
