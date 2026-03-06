af6b923d90f02d8c1977f94ed525bdb3ec876ab4
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-06 13:05:58 -0500

    Update 2.10.py

--- dietel-problems/2.10.py
+++ dietel-problems/2.10.py
@@ -1,1 +1,31 @@
-x = 4
+x1 = input("What's class 1? ")
+x2 = int(input("What's the grade? "))
+
+y1 = input("What's class 2? ")
+y2 = int(input("What's the grade? "))
+
+z1 = input("What's class 3? ")
+z2 = int(input("What's the grade? "))
+
+lc = x1
+lowest = x2
+
+if lowest > y2:
+   lowest = y2 
+   lc = y1
+if lowest > z2:
+   lowest = z2
+   lc = z1
+gc = z1
+greatest = z2
+
+if greatest < y2:
+   greatest = y2 
+   gc = y1
+
+if greatest < x2:
+   greatest = x2 
+   gc = x1
+
+print(lc,lowest)
+print(gc,greatest)
