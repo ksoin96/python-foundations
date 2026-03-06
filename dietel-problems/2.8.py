1ba41aff354a2d74fbc862d3006b31d45d922d91
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-05 23:19:20 -0500

    Add files via upload

---
+++ dietel-problems/2.8.py
@@ -0,0 +1,10 @@
+#this moslty a problem using escape sequences from Python
+print("Hour\tNumber of Bacteria")
+x=0
+print(f'{x}\t{200*2**x}')
+x=5
+print(f'{x}\t{200*2**x}')
+x=10
+print(f'{x}\t{200*2**x}')
+x=15
+print(f'{x}\t{200*2**x}')
