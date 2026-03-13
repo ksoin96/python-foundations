3e6df19e7e6e8abd08399144cce320b0aac36bf2
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-13 06:26:21 -0400

    Add files via upload

---
+++ python-learning/ch2.1.py
@@ -0,0 +1,15 @@
+#variables store values for later use
+x = 7
+#variable is x and value is 7
+#"x=7" is an assignment statement that reads x is assigned the value 7
+#assignment symbol "="  give the variable a value
+y = 7
+total = x+y
+# "+" is  an addition operator with also being a binary since it require two operands "x" and "7"
+#The right side of the = symbol always executes first, then the result is assigned to the variable on the symbol’s left side.
+
+#A variable name, such as x, is an identifier. Each identifier may consist of letters, digits and underscores (_) but may not begin with a digit. Python is case sensitive, so number andNumber are different identifiers because one begins with a lowercase letter and the other begins with an uppercase letter
+
+g = type(x)
+# the type function determines the data type of the variable
+#A function performs a task when you call it by writing its name, followed by parentheses, (). The parentheses contain the function’s argument—the data that the type function needs to perform its task.
