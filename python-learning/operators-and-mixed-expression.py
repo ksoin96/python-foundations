8b153f2c6ebacd62c8c4f3a5cf57dc91e4814b00
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-14 22:49:21 -0400

    Add Python script for demonstrating operators
    This script demonstrates various operators in Python, including multiplication, exponentiation, division, floor division, and the remainder operator. It also includes a mixed expression and notes that division by zero will raise an exception.

---
+++ python-learning/operators-and-mixed-expression.py
@@ -0,0 +1,19 @@
+a = 7*4
+print(a)
+# multiplication operator
+b = 2**3
+print(b)
+# exponentation operator
+c = 7/4
+print(c)
+# True division gives a floating point number
+d = 7//4
+print(d)
+# Floor division rounds to the nearest integer
+e = 7/0
+print(e)
+#the code will cause an exception
+f = 16%3
+# remainder operator gives the remainder
+x = 6 + 1.6
+# a mixed expression includes a float and int but always result a float
