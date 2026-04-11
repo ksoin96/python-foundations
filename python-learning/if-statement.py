bc9bb401ce16678c88c3722375d2dfd549737ce5
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-04-11 19:30:25 -0400

    Implement grade evaluation using if-statement
    Add if-statement example for grade evaluation

--- python-learning/if-statement.py
+++ python-learning/if-statement.py
@@ -1,1 +1,5 @@
-
+grade = 93
+# if-statements depend on whether the condition is True or False.
+if grade >= 90:
+  print("It's an A!")
+# if the condition is True then the indented statement(s) are executed
