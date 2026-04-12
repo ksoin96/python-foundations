2a3866f26fad6923a56c0175bf29a012202c5187
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-04-11 21:49:47 -0400

    Add files via upload

---
+++ python-learning/while-statements.py
@@ -0,0 +1,6 @@
+# while statemens are a type of loop statement that repeate while the condition remains true
+x = 50
+while x > 0:
+    #these statements are executed while x is greater than 0
+    x = x-10
+    print(x)
