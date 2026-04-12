6b9c5cecd855f7e82f41f761a631fbeb212ad043
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-04-11 21:09:23 -0400

    Add files via upload

---
+++ python-learning/if-elif-else-statments.py
@@ -0,0 +1,12 @@
+x = 78
+if x >= 90:
+    print("A")
+#in this type of statement elif is dealt with if the "if statements turn out to be false"
+elif x >= 80:
+    print("B")
+elif x >= 70:
+    print("C")
+elif x>= 60:
+    print("D")
+else:
+    print("F")
