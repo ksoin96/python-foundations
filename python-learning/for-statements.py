7a936c4c8ab3b303013d413bd0fa1e09080a0438
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-04-11 22:26:13 -0400

    Add files via upload

---
+++ python-learning/for-statements.py
@@ -0,0 +1,5 @@
+#for statements are a type of loop statement where an action is repeated for each item in a sequence of items
+
+for x in range(1,5) :
+    # an "x" is printed out for 1 to 4
+    print("x")
